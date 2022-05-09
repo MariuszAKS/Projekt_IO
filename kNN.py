9078930f587ac7a755392ed516557c766eda792b
domi-pro <103370112+domi-pro@users.noreply.github.com>
2022-05-09 23:13:40 +0200

    Add files via upload

---
+++ kNN.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn import metrics
+#importuję plik z cechami do dataframe
+cechy = pd.read_csv("cechy.csv", decimal=".", sep=";")
+#usuwam dwie pierwsze kolumny, aby zostały tylko komulny z cechami
+X = cechy.drop(["-- Rodzaj --", "-- Numer --"], axis=1)
+X = X.values
+y = cechy["-- Rodzaj --"]
+y = y.values
+#dzielę zbiory na podzbiór treningowy i podzbiór testowy
+X_Train, X_Test, y_Train, y_Test = train_test_split(X, y, test_size=0.5)
+#klasyfikacja kNN
+knn = KNeighborsRegressor(n_neighbors=3)
+knn.fit(X_Train, y_Train)
+#przewidywany rodzaj
+y_pred = knn.predict(X_Train)
+#accuracy (zgodność dopasowania)
+#print("Accuracy:", metrics.accuracy_score(y_Test, y_pred)) #zakomentowane bo wyrzuca jakiś błąd
