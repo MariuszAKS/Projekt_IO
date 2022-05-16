0b34fc7f6b4688e374a0c8e063fc6788fe2d3c5e
MariusAKS <102185364+MariusAKS@users.noreply.github.com>
2022-05-16 21:45:24 +0200

    Update main.py

--- aplikacja-alpha/main.py
+++ aplikacja-alpha/main.py
@@ -1,10 +1,3 @@
-# import csv
-# import numpy as np
-#
-# import analiza_jednego
-# import analiza_wszystkie_cechy
-# import klasyfikacja_rf
-# import klasyfikacja_rf_featselect
 import generowanie_cech
 import klasyfikacja_obrazu
 
@@ -32,31 +25,3 @@ elif wynik == 5:
     print('Rhizobium')
 else:
     print('Cos poszlo nie tak!')
-
-
-
-
-
-
-
-
-# klasyfikacja_rf_featselect.klasyfikacja_rf_main()
-
-# sciezka_do_pliku = 'Bakterie\\Bradyrhizobium\\b00001.png'
-
-# analiza_jednego.Analizuj(sciezka_do_pliku)
-# analiza_wszystkie_cechy.oblicz_cechy()
-
-
-# with open('rf.csv', 'w', newline='') as plik:
-#     w = csv.writer(plik, delimiter=';')
-#
-#     nazwy_kolumn, waznosc_cechy, accuracy = klasyfikacja_rf.klasyfikacja()
-#     w.writerow(np.concatenate((['Accuracy'], nazwy_kolumn), axis=None))
-#     wiersz = np.concatenate((accuracy, waznosc_cechy), axis=None)
-#     w.writerow(wiersz)
-#
-#     for i in range(0, 199):
-#         _, waznosc_cechy, accuracy = klasyfikacja_rf.klasyfikacja()
-#         wiersz = np.concatenate((accuracy, waznosc_cechy), axis=None)
-#         w.writerow(wiersz)
