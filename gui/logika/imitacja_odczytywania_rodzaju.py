40095eb367bf812a7a23c35cda87a008f6f60dbd
Adiro <adiro.rostek@gmail.com>
2022-05-17 00:32:11 +0200

    Przypisane rodzaje są teraz wyświetlane w gui

--- gui/logika/imitacja_odczytywania_rodzaju.py
+++ gui/logika/imitacja_odczytywania_rodzaju.py
@@ -2,45 +2,48 @@ import random as rand
 from time import sleep
 from logika.wybor_plikow import wybierz_pliki
 from PyQt6.QtCore import QThread, QObject, pyqtSignal
+from ui.element_listy import dodaj_pozycje
+from ui.gui import Ui_MainWindow
 
 lista = ["red", "blue", "green", "brown", "orange", "purple"]
 
 
-def ustaw_dodawanie(ui):
+def ustaw_dodawanie(ui: Ui_MainWindow):
     ui.watki = []
     ui.pushButton.clicked.connect(lambda: _analizuj_pliki(ui))
 
-def _analizuj_pliki(ui):
+def _analizuj_pliki(ui: Ui_MainWindow):
     sciezki = wybierz_pliki(ui.widget_2)
     for sciezka in sciezki:
-        ui.watki.append(_Watek(sciezka))
+        watek = _Watek(sciezka, ui)
+        ui.watki.append(watek)
 
 class _Watek():
-    def __init__(self, sciezka: str) -> None:
-        self.thread = QThread()
-        self.worker = _Podproces(sciezka)
-        self.worker.moveToThread(self.thread)
-        self.thread.started.connect(self.worker.run)
-        self.worker.finished.connect(self.thread.quit)
-        self.worker.finished.connect(self.worker.deleteLater)
-        self.thread.finished.connect(self.thread.deleteLater)
-        self.thread.start()
+    def __init__(self, sciezka: str, ui: Ui_MainWindow) -> None:
+        self.watek = QThread()
+        self.proces = _Proces(sciezka)
+        self.proces.moveToThread(self.watek)
+        self.watek.started.connect(self.proces.run)
+        self.proces.zakonczony.connect(lambda: dodaj_pozycje(ui, self))
+        self.proces.zakonczony.connect(self.watek.quit)
+        self.proces.zakonczony.connect(self.proces.deleteLater)
+        self.watek.finished.connect(self.watek.deleteLater)
+        self.watek.start()
 
 
-class _Podproces(QObject):
-    finished = pyqtSignal()
+class _Proces(QObject):
+    zakonczony = pyqtSignal()
 
     def __init__(self, sciezka: str) -> None:
         super().__init__()
         self.sciezka = sciezka
         self.lista = lista
+        self.rodzaj = None
 
     def run(self):
-        rodzaj = _zwroc_rodzaj(self.sciezka)
-        # zapisz gdzies rodzaj
-        # verticalLayout_2.addWidget(ElementListy(self.scrollAreaWidgetContents))
-        print(rodzaj)
-        self.finished.emit()
+        self.rodzaj = _zwroc_rodzaj(self.sciezka)
+        print(self.rodzaj)
+        self.zakonczony.emit()
 
 
 def _zwroc_rodzaj(sciezka: str) -> str:
--- gui/ui/element_listy.py
+++ gui/ui/element_listy.py
@@ -1,20 +1,30 @@
 from PyQt6.QtWidgets import *
 from PyQt6.QtCore import Qt
+from ui.gui import Ui_MainWindow
 
 class ElementListy(QWidget):
-    def __init__(self, rodzic, rodzaj: str) -> None:
+    def __init__(self, rodzic, nazwa: str, zdjecie: str, rodzaj: str) -> None:
         super().__init__(rodzic)
         rozstawienie = QHBoxLayout(self)
         rozstawienie.setSizeConstraint(QLayout.SizeConstraint.SetDefaultConstraint)
 
-        nazwa = QLabel("Tutaj zdjęcie!", self)
-        zdjecie = QLabel("Zdjecie1.jpg", self)
+        nazwa = QLabel(nazwa, self)
+        zdjecie = QLabel(zdjecie, self)
         rodzaj = QLabel(rodzaj, self)
 
         nazwa.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignCenter)
         zdjecie.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignCenter)
         rodzaj.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignCenter)
 
-        rozstawienie.addWidget(nazwa)
         rozstawienie.addWidget(zdjecie)
+        rozstawienie.addWidget(nazwa)
         rozstawienie.addWidget(rodzaj)
+
+
+def dodaj_pozycje(ui: Ui_MainWindow, watek):
+
+    nazwa = watek.proces.sciezka
+    zdjecie = "zdjecie"
+    rodzaj = watek.proces.rodzaj
+
+    ui.verticalLayout_2.addWidget(ElementListy(ui.scrollAreaWidgetContents, nazwa, zdjecie, rodzaj))
