8493e75f96b617758456dc461c7f74e4f14d5053
Adiro <adiro.rostek@gmail.com>
2022-05-17 01:54:41 +0200

    Dodano podglad zdjec na liscie

--- gui/ui/element_listy.py
+++ gui/ui/element_listy.py
@@ -1,28 +1,27 @@
 from PyQt6.QtWidgets import *
 from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QPixmap, QResizeEvent
 from ui.gui import Ui_MainWindow
 
 class ElementListy(QWidget):
-    def __init__(self, rodzic, nazwa: str, zdjecie: str, rodzaj: str) -> None:
+    def __init__(self, rodzic, sciezka: str, zdjecie: str, rodzaj: str) -> None:
         super().__init__(rodzic)
-        # rozstawienie = QHBoxLayout(self)
+
         rozstawienie = QGridLayout(self)
         rozstawienie.setSizeConstraint(QLayout.SizeConstraint.SetDefaultConstraint)
 
-        nazwa = QLabel(nazwa, self)
-        zdjecie = QLabel(zdjecie, self)
+        zdjecie = _Podglad_zdjecia(sciezka, self)
+        sciezka = QLabel(sciezka, self)
         rodzaj = QLabel(rodzaj, self)
 
-        nazwa.setSizePolicy(QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Preferred)
+        sciezka.setSizePolicy(QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Preferred)
         zdjecie.setSizePolicy(QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Preferred)
         rodzaj.setSizePolicy(QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Preferred)
 
-        nazwa.setWordWrap(True)
-        zdjecie.setWordWrap(True)
+        sciezka.setWordWrap(True)
         rodzaj.setWordWrap(True)
 
-
-        nazwa.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignCenter)
+        sciezka.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignCenter)
         zdjecie.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignCenter)
         rodzaj.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignCenter)
 
@@ -31,8 +30,24 @@ class ElementListy(QWidget):
         rozstawienie.setColumnStretch(2, 1)
 
         rozstawienie.addWidget(zdjecie)
-        rozstawienie.addWidget(nazwa)
+        rozstawienie.addWidget(sciezka)
         rozstawienie.addWidget(rodzaj)
+
+
+class _Podglad_zdjecia(QLabel):
+    def __init__(self, sciezka, rodzic):
+        super().__init__(rodzic)
+        self.__pixmapa = QPixmap(sciezka)
+        self.setPixmap(self.__pixmapa.scaledToWidth(200))
+        self.show()
+
+
+    def resizeEvent(self, resizeEvent: QResizeEvent) -> None:
+        margines = 1  # pixmapa nie moze zajmowac calej dostepnej przestrzeni bo layout nigdy nie bedzie chcial jej zmniejszac
+        zeskalowana_pixmapa = self.__pixmapa.scaledToWidth(resizeEvent.size().width()-margines)
+        self.setPixmap(zeskalowana_pixmapa)
+        self.show()
+        return super().resizeEvent(resizeEvent)
 
 
 
