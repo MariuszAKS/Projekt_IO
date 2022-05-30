b7cd9cb782e1245575e80fea6342619940626bb5
Dawryb <102424537+Dawryb@users.noreply.github.com>
2022-05-30 21:43:18 +0200

    Update kNN.py

--- analiza_obrazu/kNN.py
+++ analiza_obrazu/kNN.py
@@ -4,19 +4,19 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn import metrics
-#importuję plik z cechami do dataframe
+# importuję plik z cechami do dataframe
 cechy = pd.read_csv("cechy.csv", decimal=".", sep=";")
-#usuwam dwie pierwsze kolumny, aby zostały tylko komulny z cechami
+# usuwam dwie pierwsze kolumny, aby zostały tylko kolumny z cechami
 X = cechy.drop(["-- Rodzaj --", "-- Numer --"], axis=1)
 X = X.values
 y = cechy["-- Rodzaj --"]
 y = y.values
-#dzielę zbiory na podzbiór treningowy i podzbiór testowy
+# dzielę zbiory na podzbiór treningowy i podzbiór testowy
 X_Train, X_Test, y_Train, y_Test = train_test_split(X, y, test_size=0.5)
-#klasyfikacja kNN
+# klasyfikacja kNN
 knn = KNeighborsRegressor(n_neighbors=3)
 knn.fit(X_Train, y_Train)
-#przewidywany rodzaj
+# przewidywany rodzaj
 y_przewidywany = knn.predict(X_Train)
 print(y_przewidywany)
 for i in range(len(y_przewidywany)):
