5f3af373eac3e59d5a228df6dae0e8e4d9453a88
Dawryb <102424537+Dawryb@users.noreply.github.com>
2022-05-30 22:05:22 +0200

    Update progowanie.py

--- analiza_obrazu/progowanie.py
+++ analiza_obrazu/progowanie.py
@@ -4,15 +4,15 @@ from skimage.color import rgb2gray
 from skimage.util import img_as_ubyte
 
 
-# klasa do generacji maski uzywajac OTSU
-# konwertuje obraz do skali szarosci (grayscale), znajduje prog (threshold(float)) i generuje maske
+# klasa do generacji maski używając OTSU
+# konwertuje obraz do skali szarości (grayscale), znajduje próg (threshold(float)) i generuje maskę
 
 class ProgowanieOTSU:
     def __init__(self) -> None:
         pass
 
-    # zwraca maske w postaci numpy array
-    # obraz = sciezka do pliku/numpy array
+    # zwraca maskę w postaci numpy array
+    # obraz = ścieżka do pliku/numpy array
     def maska(self, obraz):
         if type(obraz) == str:
             skala_szarosci = rgb2gray(skimage.io.imread(fname=obraz))
@@ -23,9 +23,9 @@ class ProgowanieOTSU:
         wynik = skala_szarosci <= prog
         return wynik
 
-    # generuje maske i zapisuje do png (true=255, false=0)
-    # obraz = sciezka do pliku/numpy array
-    # sciezka_wynik = sciezka do zapisu
+    # generuje maskę i zapisuje do png (true=255, false=0)
+    # obraz = ścieżka do pliku/numpy array
+    # sciezka_wynik = ścieżka do zapisu
     def zapisz_maske(self, obraz, sciezka_wynik):
         wynik = self.maska(obraz)
         skimage.io.imsave(sciezka_wynik, img_as_ubyte(wynik))
