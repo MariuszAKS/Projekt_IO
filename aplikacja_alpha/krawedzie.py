19587454219517d40b3272018749e12d931a41c0
Maciegu <66634598+Maciegu@users.noreply.github.com>
2022-05-30 21:40:52 +0200

    Update krawedzie.py

--- aplikacja_alpha/krawedzie.py
+++ aplikacja_alpha/krawedzie.py
@@ -6,7 +6,7 @@ import numpy as np
 
 # klasa do wykrycia krawedzi, obliczenia maski krawedzi i cech kolorymetrycznych krawedzi
 # arg do konstruktora to obraz w formie numpy array/scierzka do pliku
-# zmien_obraz() zby zmienic aktualnie analizowany obraz
+# zmien_obraz() zeby zmienic aktualnie analizowany obraz
 class Krawedzie:
     def __init__(self, obraz) -> None:
         self.obr_kolor = obraz
