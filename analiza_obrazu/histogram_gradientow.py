863ab29ccf186b14cda5fc6398400105e07846bc
Dawryb <102424537+Dawryb@users.noreply.github.com>
2022-05-30 21:42:13 +0200

    Update histogram_gradientow.py

--- analiza_obrazu/histogram_gradientow.py
+++ analiza_obrazu/histogram_gradientow.py
@@ -4,11 +4,11 @@ from skimage.feature import hog
 from skimage.color import rgb2gray
 
 
-# klasa tworzaca histogram zorientowanych gradientow (Histogram of oriented gradients) dla obrazu
-# histogram ten to masywny, jednowymiarowy wektor cech (rozmiar kilku tysiecy), wiec mozna sprobowac
-# skompresowac te cechy wyciagajac z nich srednia/odchylenie standardowe (trudno powiedziec czy beda one zawierac wartoscowe informacje)
-# klasa jako argument konstruktora bieze obraz (numpy array)
-# zmien_obraz() aby zmienic analizowane zdjecie
+# klasa tworząca histogram zorientowanych gradientów (Histogram of oriented gradients) dla obrazu
+# histogram ten to masywny, jednowymiarowy wektor cech (rozmiar kilku tysięcy), więc można spróbować
+# skompresować te cechy wyciągając z nich średnią/odchylenie standardowe (trudno powiedzieć czy będą one zawierać wartościowe informacje)
+# klasa jako argument konstruktora bierze obraz (numpy array)
+# zmien_obraz(), aby zmienić analizowane zdjęcie
 class CechyHOG:
     def __init__(self, obraz) -> None:
         if type(obraz) == str:
@@ -16,7 +16,7 @@ class CechyHOG:
         else:
             obr = rgb2gray(obraz)
         cechy_wszystkie = hog(obr, orientations=8, pixels_per_cell=(8, 8), cells_per_block=(1, 1), feature_vector=False)
-        # dla kazdego bloku znajdz najbardziej wplywowa orientacje
+        # dla kazdego bloku znajdź najbardziej wpływową orientacje
         cechy_zredukowane = np.zeros((cechy_wszystkie.shape[0], cechy_wszystkie.shape[1]))
         for i in range(cechy_wszystkie.shape[0]):
             for j in range(cechy_wszystkie.shape[1]):
@@ -29,18 +29,18 @@ class CechyHOG:
         else:
             obr = rgb2gray(obraz)
         cechy_wszystkie = hog(obr, orientations=8, pixels_per_cell=(8, 8), cells_per_block=(1, 1), feature_vector=False)
-        # dla kazdego bloku znajdz najbardziej wplywowa orientacje
+        # dla kazdego bloku znajdź najbardziej wpływową orientacje
         cechy_zredukowane = np.zeros((cechy_wszystkie.shape[0], cechy_wszystkie.shape[1]))
         for i in range(cechy_wszystkie.shape[0]):
             for j in range(cechy_wszystkie.shape[1]):
                 cechy_zredukowane[i, j] = np.argmax(cechy_wszystkie[i, j, 0, 0, :]) / 8
         self.cechy = cechy_zredukowane.ravel()
 
-    # zwraca srednia gradientow (float)
+    # zwraca średnią gradientów (float)
     def srednia(self):
         return self.cechy.mean()
 
-    # zwraca odchylenie standardowe gradientow (float)
+    # zwraca odchylenie standardowe gradientów (float)
     def std(self):
         return self.cechy.std()
 
